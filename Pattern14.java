77f8e3d5e1a1acf4857235751044fe8fd31f4c05
SUMAN RANA <68540296+Suman1999-hub@users.noreply.github.com>
2021-12-08 01:18:30 +0530

    Add files via upload

---
+++ Pattern14.java
@@ -0,0 +1,38 @@
+//Pattern 14
+/*
+ * Input:
+ 		5
+   Output:
+		1 
+		1 0 
+		1 0 1 
+		1 0 1 0 
+		1 0 1 0 1 
+ */
+package Pattern;
+import java.util.*;
+public class Pattern14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int row=sc.nextInt();
+		int pos=1;
+		for(int i=1;i<=row;i++) {
+			for(int j=1;j<=i;j++) {
+				if(j%2==1) {
+					System.out.print("1 ");
+				}
+				else {
+					System.out.print("0 ");
+				}
+				pos++;
+				
+			}
+			System.out.println();
+		}
+
+
+	}
+
+}
